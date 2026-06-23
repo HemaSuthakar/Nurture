d12fd08e0a723270bfedcce4b3aaaaea199b0c94
HemaSuthakar <hemasuthakar1226@gmail.com>
2026-06-23 22:38:16 +0530

    First 10 Program Completion

---
+++ CORE JAVA/DataTypeDemo.java
@@ -0,0 +1,14 @@
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        int myInt = 42;                            
+        float myFloat = 3.14f;                      
+        double myDouble = 19.99;                  
+        char myChar = 'A';                         
+        boolean myBoolean = true;                  
+        System.out.println("Integer value: " + myInt);
+        System.out.println("Float value: " + myFloat);
+        System.out.println("Double value: " + myDouble);
+        System.out.println("Character value: " + myChar);
+        System.out.println("Boolean value: " + myBoolean);
+    }
+}
---
+++ CORE JAVA/EvenOrOddChecker.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EvenOddChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+
+        // The modulus operator % returns the remainder of a division.
+        // If a number divided by 2 has a remainder of 0, it is even.
+        if (number % 2 == 0) {
+            System.out.println(number + " is an Even number.");
+        } else {
+            System.out.println(number + " is an Odd number.");
+        }
+
+        scanner.close();
+    }
+}
